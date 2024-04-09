3ac5ed48d39356ea30acb63c0b330f016e5fa2b1
Dmitry Starov <dmitry.staroff@gmail.com>
2024-04-09 07:54:41 +0200

    Create entry-point

--- src/ym2s/cli/__init__.py
+++ src/ym2s/cli/__init__.py
@@ -1,1 +1,7 @@
-"""YM2S CLI package."""
+"""YM2S CLI."""
+
+from ym2s.cli.grp_ym2s import cli
+
+
+def main():
+    cli.main()
--- src/ym2s/main.py
+++
@@ -1,4 +0,0 @@
-from ym2s.cli.grp_ym2s import cli
-
-if __name__ == '__main__':
-    cli.main()
